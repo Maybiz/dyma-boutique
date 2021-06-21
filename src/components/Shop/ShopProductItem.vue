dbd1348eeeee2798c86f4297cfbecccb95ebb0d5
Maybiz <maxime.julliant@laposte.net>
2021-06-21 07:04:44 +0200

    end chapter 8

--- src/components/Shop/ShopProductItem.vue
+++ src/components/Shop/ShopProductItem.vue
@@ -1,9 +1,36 @@
 <template>
-  <div class="w-25 border product-container">Item</div>
+  <div class="w-25 p-3 product-container d-flex flex-column">
+    <div class="d-flex flex-row justify-content-center">
+      <img style="height: 150px" :src="product.img" />
+    </div>
+    <div>
+      <h3>{{ product.title }}</h3>
+      <hr />
+    </div>
+    <div>
+      <p>{{ product.description }}</p>
+    </div>
+    <div>
+      <span>{{ product.price }}</span>
+      <button class="btn btn-primary btn-sm float-right">Commander</button>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      product: {
+        img: "https://www.journaldugeek.com/content/uploads/2018/07/macbook-air.jpg",
+        title: "MacBook",
+        description:
+          "D'une finesse et d'une légèreté hors du commun, le MacBook Pro est encore plus rapide et plus puissant qu'avant.",
+        price: "1500 €",
+      },
+    };
+  },
+};
 </script>
 
 <style>
